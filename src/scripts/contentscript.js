dba600fdc4681cf80175969659fb0201593835d8
Alevale <alexvales111@gmail.com>
2017-12-17 00:37:27 +0100

    arrange better the project

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,66 +1,73 @@
 'use-strict';
 
 import {makeCallForCoinValue} from './utils/api';
-// NOTE: This is needed because Jquery is doing weird things
-// in the communication with the page... 20171215:Alevale
+// NOTE: This is needed because Jquery is doing weird
+// things when required in an extension 20171215:Alevale
 let $ = require('jquery');
 
-let makeTableFullWith = () => {
-    $('.width_62_percent').addClass('width_full');
-    $('.width_62_percent').removeClass('width_62_percent');
+// NOTE: Make the content of the page full width to have
+// enough space for the new info 20171217:Alevale
+let makeContentFullWidth = () => {
+    let el = $('.width_62_percent');
+    el.addClass('width_full');
+    el.removeClass('width_62_percent');
 };
 
-let getCoinValuesFromTable = () => {
-    let table = $('tbody');
+// NOTE: Evaluate if the page is the one we want to target 20171217:Alevale
+let verifyTheTargetedTable = () => {
+    return $('thead>tr>th').first().text() === 'Coin';
+};
 
-    if (table) {
-        let tableChildren = table.children();
+// NOTE: Create the elements to fill later while iterating in the API retrieved loop 20171217:Alevale
+let makeUIPageArrangements = () => {
+    let div = $('<div></div>');
+    div.append($('<h4 class="totalmoneydisclaimer"></h4>'));
+    $($('tbody').parent().parent()).prepend(div);
+    $('thead>tr').append($('<th class="tableheaderComment"></th>'));
+    makeContentFullWidth();
+};
+
+// NOTE: This function has all the logic currently to arrange the page and call the coins 20171217:Alevale
+let getCoinValuesFromTable = () => {
+    if (verifyTheTargetedTable()) {
+
+        makeUIPageArrangements();
+
         let totalMoney = 0;
         let totalMoney24Ago = 0;
 
-        let div = $('<div></div>');
-        div.append($('<h4 class="totalmoney"></h4>'));
-        $(table.parent().parent()).prepend(div);
+        $('tbody').children().each((key, r) => {
 
-        $('thead>tr').append($('<th class="explanation"></th>'));
+            let row = $(r);
+            let coinName = row.text().split('\n')[1];
+            let coins = row.text().split('\n')[2];
+            let match = coins.match(/(.*) \((.*)\)/);
+            let totalCoins = match ? Number(match[1]) + Number(match[2]) : Number(coins);
 
-        tableChildren.each((k, v) => {
-            let val = $(v);
+            makeCallForCoinValue(coinName).then((res) => {
+                let value = res.price_usd * totalCoins;
+                let value24Ago = ((-res.percent_change_24h / 100) + 1 ) * res.price_usd * totalCoins;
 
-            if (val.is('tr') && val.text().match(/Manage Wallet/)) {
+                totalMoney += value;
+                totalMoney24Ago += value24Ago;
 
-                let coinName = val.text().split('\n')[1];
-                let coins = val.text().split('\n')[2];
-                let match = coins.match(/(.*) \((.*)\)/);
-                let totalCoins = match ? Number(match[1]) + Number(match[2]) : Number(coins);
+                $('.totalmoneydisclaimer').text(`Total value now $${totalMoney.toFixed(2)}. 
+                    This same amount of coins 24 hours ago was valued in $${totalMoney24Ago.toFixed(2)}. 
+                    The difference is $${(totalMoney - totalMoney24Ago).toFixed(2)}`);
+                $('.tableheaderComment').text(`Coin values in real time`);
 
-                makeCallForCoinValue(coinName).then((res) => {
-                    let value = res.price_usd * totalCoins;
-                    let value24Ago = ((-res.percent_change_24h / 100) + 1 ) * res.price_usd * totalCoins;
+                let td = $('<td></td>');
 
-                    totalMoney += value;
-                    totalMoney24Ago += value24Ago;
+                td.append($('<tr></tr>').text(`Credited coins ${totalCoins}`));
+                td.append($('<tr></tr>').text(`USD ($${value.toFixed(2)})`));
+                td.append($('<tr></tr>').text(`USD 24 ago ($${value24Ago.toFixed(2)})`));
+                td.append($('<tr></tr>').text(`${coinName} price 24h ago $${(((-res.percent_change_24h / 100) + 1 ) * res.price_usd).toFixed(2)}`));
+                td.append($('<tr></tr>').text(`${coinName} price now $${Number(res.price_usd).toFixed(2)}, ${res.percent_change_24h}%`));
 
-                    $('.totalmoney').text(`Total value now (${totalMoney.toFixed(2)} $), 
-                    this same amount of coins 24 hours ago was (${totalMoney24Ago.toFixed(2)} $), 
-                    by keeping the coin unchanged you made (${(totalMoney - totalMoney24Ago).toFixed(2)} $)`);
-                    $('.explanation').text(`Coin values in real time`);
-
-                    let td = $('<td></td>');
-
-                    td.append($('<tr></tr>').text(`Coins ${totalCoins}`));
-                    td.append($('<tr></tr>').text(`USD (${value.toFixed(2)} $)`));
-                    td.append($('<tr></tr>').text(`USD 24 ago (${value24Ago.toFixed(2)} $)`));
-                    td.append($('<tr></tr>').text(`${coinName} valuation 1h ago ${res.percent_change_1h}%`));
-                    td.append($('<tr></tr>').text(`${coinName} valuation 24h ago ${res.percent_change_24h}%`));
-
-                    val.append(td);
-                    makeTableFullWith();
-                });
-            }
+                row.append(td);
+            });
 
         });
-
     }
 };
 
